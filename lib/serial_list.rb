09c0386add18a5e880a6aecbfa854031d1fb3bf7
James Van Mil <james.vanmil@gmail.com>
2015-06-22 10:42:47 -0400

    move classes to lib

---
+++ lib/serial_list.rb
@@ -0,0 +1,46 @@
+module SerialList
+  require 'bundler/setup'
+  require 'active_sierra_models'
+
+  def self.all_potential_serial_orders
+    (serial_orders_by_status_code + (serial_orders_by_funds)).uniq
+  end
+
+  def self.serial_orders_by_status_code
+    OrderView.where(
+      order_status_code: serial_status_codes,
+      ocode1: ["u", "h"] ## Jurisdiction 
+    )
+  end
+
+  def self.get_codes_from_funds
+    ## We're using these to look up orders via OrderRecordCmf, which needs the codes as 5-digit strings
+    (FundMaster.where(code: all_fund_list).collect { |f| "%05d" % f.code_num}).uniq
+  end
+
+  def self.serial_orders_by_funds
+    ## This catches all the orders against serial funds with a monograph status code
+    OrderView.joins(:order_record_cmf).where(
+      order_record_cmf: { fund_code: get_codes_from_funds }).where(
+      order_status_code: monograph_status_codes,
+      ocode1: jurisdiction_codes
+    )
+  end
+
+  def self.serial_status_codes
+    ["c", "f", "d", "e", "g"]
+  end
+
+  def self.monograph_status_codes
+    ["a", "o", "q"]
+  end
+
+  def self.jurisdiction_codes
+    ["u", "h"]
+  end
+
+  def self.all_fund_list(fund_array = YAML::load(File.open('script_configuration.yml'))["funds"])
+    ## Returns all funds unless another array is passed
+    fund_array
+  end
+end
