d8f28535eca801c76b087e9455152db39823b7b5
James Van Mil <james.vanmil@gmail.com>
2015-06-25 15:19:07 -0400

    Add destroy to script

--- cereal.rb
+++ cereal.rb
@@ -2,6 +2,8 @@ require_relative 'lib/serial_list'
 require_relative 'lib/list'
 require_relative 'lib/titles'
 
+Titles.destroy_all
+
 SerialList.all_potential_serial_orders.each do |order_view|
   new_title = Titles.new
   new_title.assign_base_fields(order_view)
@@ -9,11 +11,6 @@ SerialList.all_potential_serial_orders.each do |order_view|
   new_title.assign_usage
   new_title.save
 end
-
-require_relative 'lib/serial_list'
-require_relative 'lib/list'
-require_relative 'lib/titles'
-
 
 list = List.new(Titles.all)
 list.worksheet; nil
