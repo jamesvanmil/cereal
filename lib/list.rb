2d8ce27d8acb3738de8d96e7ce5e88fadadc6890
James Van Mil <james.vanmil@gmail.com>
2015-06-25 08:47:00 -0400

    Add reporting mappings

--- lib/list.rb
+++ lib/list.rb
@@ -42,17 +42,17 @@ class List
   def spreadsheet_mapping(title)
     [
       { order_number: title.order_number },
+      { vendor: title.vendor },
+      { acqusition_type: mapped_acqusition_type(title.acqusition_type) },
+      { split: title.split },
+      { fund: title.fund },
+      { selector: title.selector },
+      { format: mapped_format(title.format) },
       { title: title.title },
       { issn1: title.issn1 },
       { issn2: title.issn2 },
       { online_access: title.online_access },
       { usage: title.usage },
-      { format: mapped_format(title.format) },
-      { fund: title.fund },
-      { selector: title.selector },
-      { vendor: title.vendor },
-      { acqusition_type: title.acqusition_type },
-      { split: title.split },
       { "FY#{fiscal_year(0)}".to_s => title.fyminus0 },
       { "FY#{fiscal_year(1)}".to_s => title.fyminus1 },
       { "FY#{fiscal_year(2)}".to_s => title.fyminus2 },
@@ -63,15 +63,29 @@ class List
 
   def mapped_format(code)
     case code
+    when 'c' then 'CD/DVD'
     when 'e' then 'electronic'
+    when 'f' then 'film'
+    when 'g' then 'maps'
+    when 'i' then 'realia'
+    when 'i' then 'looseleaf'
+    when 'm' then 'microfilm'
+    when 'n' then 'newspaper'
+    when 'p' then 'print+online'
+    when 'q' then 'microfiche'
     when 'u' then 'print'
+    when 'v' then 'video'
+    when 'x' then 'mixed format'
     when 'w' then 'score'
+    when 'z' then 'other'
     else code 
     end
   end
 
   def mapped_acqusition_type(code)
     case code
+    when 'b' then 'prepaid'
+    when 'c' then 'comes with'
     when 'p' then 'purchase'
     else code 
     end
